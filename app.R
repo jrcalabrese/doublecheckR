a22b04784dc261566f9b6a208a413a1188df8b95
‘Julianna <juliannarosecalabrese@gmail.com>
2022-06-01 12:02:13 -0400

    first working version, changed purpose of app
    no longer a replica of Keith Chan's compare-o-matic; now it's purpose is to resolve disputes with behavioral coding

---
+++ app.R
@@ -0,0 +1,106 @@
+library(shiny)
+library(shinythemes)
+library(data.table)
+library(DT)
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(rsconnect)
+
+ui <- navbarPage("doublecheckR by @jrosecalabrese", 
+                  theme = shinytheme("flatly"), 
+                  tabPanel("Upload", 
+                           titlePanel("Upload your datafiles"),
+                           sidebarLayout(
+                             sidebarPanel(
+                               
+                               ## File 1
+                               fileInput('file1', 'Data Entry #1',
+                                         accept = c(".xlsx")),
+                               tags$hr(),
+                               
+                               ## File 2
+                               fileInput('file2', 'Data Entry #2',
+                                         accept = c(".xlsx")),
+                               tags$hr(),
+                               downloadButton("download")
+                               
+                             ),
+                             
+                             mainPanel(
+                               #DT::dataTableOutput("contents"),
+                               DT::DTOutput("print"))
+                               #verbatimTextOutput("print"))
+                           )
+                  ),
+)
+
+server <- function(input, output, session) {
+  
+  df1 <- reactive({
+    inFile <- input$file1
+    if (is.null(input$file1))
+      return(NULL)
+    readxl::read_excel(inFile$datapath) %>% select(Period:ObjEng) %>% filter(ObjEng!=88) %>%
+      mutate(ClockStop = format(ymd_hms(ClockStop), "%H:%M:%S")) %>%
+      mutate(ClockStart = format(ymd_hms(ClockStart), "%H:%M:%S"))
+  })
+  
+  df2 <- reactive({
+    inFile <- input$file2
+    if (is.null(input$file2)) 
+      return(NULL) 
+    readxl::read_excel(inFile$datapath) %>% select(Period:ObjEng) %>% filter(ObjEng!=88) %>%
+      mutate(ClockStop = format(ymd_hms(ClockStop), "%H:%M:%S")) %>%
+      mutate(ClockStart = format(ymd_hms(ClockStart), "%H:%M:%S"))
+  })
+
+  vals <- reactiveValues(x = NULL)
+  
+  observe({
+
+    req(df1())
+    req(df2())
+    
+    tbl_diffs <- which(df1() != df2(), arr.ind = TRUE)
+    tbl_compare <- df2() %>% DT::datatable(selection = 'none', rownames = FALSE, edit = TRUE)
+    for (i in seq_len(nrow(tbl_diffs))) {
+      tbl_compare <- tbl_compare %>%
+        formatStyle(
+          columns = tbl_diffs[[i, "col"]], 
+          backgroundColor = styleRow(tbl_diffs[[i, "row"]], c('yellow')))
+    } 
+    vals$x <- tbl_compare
+  })
+  
+  output$print <- DT::renderDT({ vals$x })
+  output$contents <- DT::renderDataTable(vals$x)
+
+  proxy <- dataTableProxy("contents")
+  
+  observeEvent(input$print_cell_edit, {
+    info = input$print_cell_edit
+    str(info)
+    i = info$row
+    j = info$col + 1
+    v = info$value
+    vals$x$x$data[i, j] <<- DT:::coerceValue(v, vals$x$x$data[i, j])
+    replaceData(proxy, vals$x$x$data, resetPaging = FALSE, rownames = FALSE)
+  })
+  
+  output$download <- downloadHandler("example.csv", 
+                                     content = function(file){
+                                       write.csv(vals$x$x$data, file, row.names = F)
+                                     },
+                                     contentType = "text/csv")
+  
+}
+
+# Don't include these lines in the final product
+# Just copy and paste them into your console
+
+# Run the application 
+#shinyApp(ui = ui, server = server)
+
+# Deploy app
+#deployApp(appName = "doublecheckR", account="jrcalabrese")
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## doublecheckR
 
-Link to the original: https://keithcchan.com/professional/data-compare-o-matic
+I created this tool to better facilitate the task of resolving coding discrepancies for behavioral coding projects. Please note that as of June 1st 2022, this tool is specifically designed for one of my lab's projects so your data will not work on it as the tool specifically selects variables present in my coding sheets. However, I plan to update this in the future so researchers can manually specify which columns/row to select. 
+
+[Link to Shiny app.](https://jrcalabrese.shinyapps.io/doublecheckR/)
